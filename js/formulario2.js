fc999ced80948e30f3c57651043c7fec33f835e0
baileytorch <baileytorch@hotmail.com>
2026-07-29 12:58:29 -0400

    Trabajando con eventos

--- js/formulario2.js
+++ js/formulario2.js
@@ -1,1 +1,13 @@
 alert("Cargando un script desde un archivo externo...");
+
+let nombre = document.getElementById('inputNombre');
+alert(nombre);
+
+function cambiarFondo(){
+    event.preventDefault();
+    document.body.style.backgroundColor = '#D8F999';
+};
+
+function cargarAlerta(){
+    alert('Cambiando el contenido del elemento HTML!');
+}
