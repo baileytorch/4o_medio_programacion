c5b5171c8a89c37cc5fa455714ba879eab887c4f
baileytorch <baileytorch@hotmail.com>
2026-07-30 15:55:34 -0400

    obteniendo datos con fetch

--- js/tablas.js
+++ js/tablas.js
@@ -1,9 +1,16 @@
-function cargarDatos(){
+async function cargarDatos(){
     alert('Cargando datos...')
     try{
-        const respuesta = fetch('https://jsonplaceholder.typicode.com/users');
-        const datos = respuesta.json();
+        const respuesta = await fetch('https://jsonplaceholder.typicode.com/users');
+        const datos = await respuesta.json();
         console.log(datos);
+        
+        let cuerpoTabla = document.getElementById('cuerpoTabla');
+        datos.forEach(usuario => {
+            const fila = document.createElement('tr');
+            fila.innerHTML = `<td>${usuario.id}</td><td>${usuario.name}</td><td>${usuario.username}</td><td>${usuario.email}</td><td>${usuario.phone}</td><td>${usuario.website}</td>`
+            cuerpoTabla.appendChild(fila);
+        });
     }catch(error){
         alert('No se han podido obtener los datos...', error);
     }
