6ae9b0b9ad03bed5218c17fd18b2d0c04376e0d5
baileytorch <baileytorch@hotmail.com>
2026-07-30 15:31:21 -0400

    Método fetch

--- js/tablas.js
+++ js/tablas.js
@@ -1,3 +1,10 @@
 function cargarDatos(){
     alert('Cargando datos...')
+    try{
+        const respuesta = fetch('https://jsonplaceholder.typicode.com/users');
+        const datos = respuesta.json();
+        console.log(datos);
+    }catch(error){
+        alert('No se han podido obtener los datos...', error);
+    }
 };
